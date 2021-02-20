64e8d9f1909b95e065d7b1dd15b96379e5e207e1
Dulatto <dulat.omarov@gmail.com>
2021-02-19 23:31:27 -0500

    routes

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,5 +1,14 @@
 const {Router} = require('express')
 const router = Router()
 
+// /api/auth/register
+router.post('register', async (req, res) =>{
+
+})
+
+// /api/auth/login
+router.post('login', async (req, res) =>{
+
+})
 
 module.exports = router
