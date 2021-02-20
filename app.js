b6d67e214c64108f9c1a33dd7c45c8eb9e35708a
Dulatto <dulat.omarov@gmail.com>
2021-02-19 23:02:40 -0500

    initial state

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const express = require('express')
 const config = require('config')
+const mongoose = require('mongoose')
 
 const app = express()
 
 
 const PORT = config.get('port') || 5000
+
+
+async function start(){
+    try {
+      await  mongoose.connect(config.get('mongoUri'), {
+          
+      })
+    } catch (e) {
+        console.log('Server error', e.message)
+        process.exit(1)
+    }
+}
+start()
 
 app.listen(PORT, ()=>console.log('App has been started on port ${PORT}...'))
