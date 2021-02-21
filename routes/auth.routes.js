c71e8e9159813d631e1ac86ccd455271f29c2120
Dulatto <dulat.omarov@gmail.com>
2021-02-20 21:18:52 -0500

    routes

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,9 +1,15 @@
-const {Router} = require('express')
+const {Router, response} = require('express')
 const router = Router()
 
 // /api/auth/register
 router.post('register', async (req, res) =>{
-
+try {
+    const {email, password}= req.body
+    
+    
+} catch (e) {
+    res.status(500).json({message:'Something are getting wrong, try again'})
+}
 })
 
 // /api/auth/login
