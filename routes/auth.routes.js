6a7c1fc357a6259a31061c38d0556f68a89189b1
Dulatto <dulat.omarov@gmail.com>
2021-02-20 22:31:09 -0500

    registration

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,11 +1,24 @@
 const {Router, response} = require('express')
+const bcrypt = require('bcryptjs')
+const User = require('../models/User')
 const router = Router()
 
 // /api/auth/register
 router.post('register', async (req, res) =>{
 try {
     const {email, password}= req.body
-    
+
+    const candidate = await User.findOne({email})
+    if (candidate){
+      return  res.status(400).json({message:'This user is already existed'})
+    }
+
+     const hashedPassword = await bcrypt.hash(password, 12)
+     const user = new User({email, password: hashedPassword})
+
+     await user.save()
+
+     res.status(201).json({ message: 'User was created'})
     
 } catch (e) {
     res.status(500).json({message:'Something are getting wrong, try again'})
