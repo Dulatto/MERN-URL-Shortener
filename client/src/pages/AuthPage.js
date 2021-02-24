f99d48fd8d6edc561d7d3a32eeb6be1ce54df568
Dulatto <dulat.omarov@gmail.com>
2021-02-23 23:55:22 -0500

    react pages

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -2,8 +2,10 @@ import React from 'react'
 
 export const AuthPage = () =>{
     return (
-        <div>
-            <h1>Auth Page</h1>
+        <div className="row">
+            <div className="col s6 offset-s3">
+                <h1>Link Shortify</h1>
+            </div>
         </div>
     )
 }
